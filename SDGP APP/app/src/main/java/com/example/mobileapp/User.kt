a44fcee128a195f389cb61bb38218cfee9e4e8f9
Mihinr <mihin.2019408@iit.ac.lk>
2022-03-30 01:54:14 +0530

    Completed the Profile Activity page

--- SDGP APP/app/src/main/java/com/example/mobileapp/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mobileapp
-
-class User(val username : String? = null, val password : String? = null, val email : String? = null) {
-//    val username : String = ""
-//    val password : String = ""
-//    val email : String = ""
-}
