cbb674c5f2ce85960726c633adadbd4a608054c9
JaniduChathumina <86459858+JaniduChathumina@users.noreply.github.com>
2022-04-02 19:00:47 +0530

    Completed Login and Signup pages

---
+++ iOS_APP/buttons/roundcolorButton.js
@@ -0,0 +1,42 @@
+import React, { Component} from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+
+
+const button = props => {
+
+    const content =(
+
+        <View style={styles.container}>
+
+            <Text style={styles.text}>{props.text}</Text>
+
+        </View>
+
+    )
+
+    return <TouchableOpacity onPress={props.onPress}>{content}</TouchableOpacity>
+
+
+}
+
+export default button
+
+const styles = StyleSheet.create({
+
+    container: {
+        padding: 6,
+        width: '100%',
+        height: '100%',
+        borderRadius: 20,
+        backgroundColor: '#D47FA6',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize:16,
+        textAlign: 'center',
+        color: 'white',
+        fontWeight: 'bold'
+    }
+
+})
